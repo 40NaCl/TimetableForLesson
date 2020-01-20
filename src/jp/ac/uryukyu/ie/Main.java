2553876f8998f51414b109752175b7ce922ba679
Shiotsuki KAITO <e195754@ie.u-ryukyu.ac.jp>
2020-01-20 19:57:25 +0900

    文字列同士の「＝＝」をつかった条件文がまずかったらしく、equalsメソッドを利用することで解決できた。

--- src/jp/ac/uryukyu/ie/Main.java
+++ src/jp/ac/uryukyu/ie/Main.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+
         boolean WontContinue = true;
+        String Show = "表示";
+        String LetItMake = "作成";
+        String Finish = "終了";
+
+
         while (WontContinue){
             System.out.println("\n\n_________________________________" +
                     "\n実行したい内容を正しく入力してください。" +
-                    "\n 【menu】"+
+                    "\n【menu】"+
                     "\n・「表示」"+
                     "\n・「作成」"+
                     "\n・「終了」");
@@ -15,15 +21,16 @@ public class Main {
             Scanner sc1 = new Scanner(System.in);
             String line = sc1.nextLine();
 
-            if(line == "表示"){
+            
+            if(line.equals(Show)){
 
-            }if(line == "作成"){
+            }else if(line.equals(LetItMake)){
 
-            }if(line == "終了"){
+            }else if(line.equals(Finish)){
                 WontContinue = false;
                 System.out.println("システムを終了中です。。。");
 
-            }else{
+            }else {
                 System.out.println("入力内容に誤りがあります。");
             }
 
